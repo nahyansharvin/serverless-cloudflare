03884bf3971cd7e2632eec5e801bd29489c3711c
Nahyan Sharvin <nskonnalath@gmail.com>
2024-06-12 10:16:23 +0530

    feat: basic request mapping

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,18 @@
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.body)
+		console.log(request.headers)
+		console.log(request.method)
+
+		if (request.method === 'GET') {
+			return Response.json({
+				message: "You sent a GET request"
+			})
+		} else {
+			return Response.json({
+				message: "You did not send a GET request"
+			})
+		}
 	},
 };
